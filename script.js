e6cb2a882701518969279967d0def7292245535d
amil-ck <amil.ck@icloud.com>
2026-04-09 20:25:38 +0100

    lets you choose category

--- script.js
+++ script.js
@@ -18,68 +18,120 @@ function seededHash(seed, N) {
   return hash % N;
 }
 
-const locations = [
-  "Circus",
-  "Shopping Mall",
-  "Bank",
-  "Police Station",
-  "Hotel",
-  "Taxi",
-  "Bowling Alley",
-  "Wedding",
-  "Zoo",
-  "University",
-  "Embassy",
-  "Ice Hockey Stadium",
-  "Pirate Ship",
-  "Polar Station",
-  "Library",
-  "Airplane",
-  "Amusement Park",
-  "Coal Mine",
-  "Service Station",
-  "Restaurant",
-  "Passenger Train",
-  "Art Gallery",
-  "Beach",
-  "Casino",
-  "Vineyard",
-  "Hospital",
-  "Theater",
-  "Supermarket",
-  "Submarine",
-  "Military Base",
-];
-
-// const locations_2 = [...locations]
-// const index = Math.floor(Math.random() * locations_2.length);
-// const value = locations_2.splice(index, 1)[0];
-// console.log(locations_2.length);
-
-// const index2 = Math.floor(Math.random() * locations_2.length);
-// const value2 = locations_2.splice(index2, 1)[0];
-// console.log(locations_2.length);
-
-
-// console.log(value);
-// console.log(value2)
-
-// for (let i = 0; i < 20; i++) console.log(i, seededHash(i));
+const lists = {
+    locations : [
+        "Circus",
+        "Shopping Mall",
+        "Bank",
+        "Police Station",
+        "Hotel",
+        "Taxi",
+        "Bowling Alley",
+        "Wedding",
+        "Zoo",
+        "University",
+        "Embassy",
+        "Ice Hockey Stadium",
+        "Pirate Ship",
+        "Polar Station",
+        "Library",
+        "Airplane",
+        "Amusement Park",
+        "Coal Mine",
+        "Service Station",
+        "Restaurant",
+        "Passenger Train",
+        "Art Gallery",
+        "Beach",
+        "Casino",
+        "Vineyard",
+        "Hospital",
+        "Theater",
+        "Supermarket",
+        "Submarine",
+        "Military Base",
+    ],
+    animals: [
+        "Wolf",
+        "Goat",
+        "Whale",
+        "Shark",
+        "Spider",
+        "Vulture",
+        "Pig",
+        "Giraffe",
+        "Eagle",
+        "Tiger",
+        "Snake",
+        "Bear",
+        "Monkey",
+        "Lion",
+        "Duck",
+        "Rhino",
+        "Kangaroo",
+        "Parrot",
+        "Dolphin",
+        "Mosquito",
+        "Pidgeon",
+        "Elephant",
+        "Zebra",
+        "Turtle",
+        "Horse",
+        "Ostrich",
+        "Housefly",
+        "Chicken",
+        "Dog",
+        "Crocodile"
+    ],
+    countries: [
+        "Sweden",
+        "Israel",
+        "United Arab Emirates",
+        "Brazil",
+        "New Zealand",
+        "Argentina",
+        "Germany",
+        "Saudi Arabia",
+        "Chile",
+        "Greece",
+        "Iran",
+        "Japan",
+        "Ethiopia",
+        "South Korea",
+        "Australia",
+        "Vietnam",
+        "Russia",
+        "Netherlands",
+        "Nigeria",
+        "Italy",
+        "Canada",
+        "United States",
+        "India",
+        "Mexico",
+        "Indonesia",
+        "United Kingdom",
+        "China",
+        "Kenya",
+        "Poland",
+        "France",
+    ]
+}
 
 function handleSubmit(event) {
     event.preventDefault(); // stops the page from reloading
 
     const playerNumber = document.getElementById("playerNumber").value - 1;
     const seed = document.getElementById("seed").value;
+    const category = document.getElementById("category").value;
 
     const imposter = seededHash(seed, 4);
 
-    const locations_2 = [...locations]
-    const index = seededHash(seed, locations_2.length);
-    const value = locations_2.splice(index, 1)[0];
+    const current_catgeory = [...lists[category]]
+    const index = seededHash(seed, current_catgeory.length);
+    const value = current_catgeory.splice(index, 1)[0];
 
-    const index2 = seededHash(seed, locations_2.length);
-    const value2 = locations_2.splice(index2, 1)[0];
+    const index2 = seededHash(seed, current_catgeory.length);
+    const value2 = current_catgeory.splice(index2, 1)[0];
 
     const location = playerNumber == imposter ? value2 : value;
     document.getElementById("output").textContent = location;
@@ -89,4 +141,10 @@ function selectPlayer(n) {
   document.getElementById("playerNumber").value = n;
   document.querySelectorAll(".player-btn").forEach(b => b.classList.remove("selected"));
   event.target.classList.add("selected");
+}
+
+function selectCategory(c) {
+    document.getElementById("category").value = c;
+    document.querySelectorAll(".category-btn").forEach(b => b.classList.remove("selected"));
+    event.target.classList.add("selected");
 }
